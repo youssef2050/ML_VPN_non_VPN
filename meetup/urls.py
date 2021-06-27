5c19f0ede913d972215734439d0e66e2cc5aac2e
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2021-06-28 01:02:05 +0300

    test

--- meetup/urls.py
+++ meetup/urls.py
@@ -2,7 +2,7 @@ from meetup.ML.KNN import KNN
 from django.urls import path
 from . import views
 urlpatterns=[
-path('index',views.index),
+path('',views.index),
 path('index/<interface>',views.runTime,name = 'run'),
 path('index/<interface>/stop',views.stopCapture,name = 'stop')
 ]
